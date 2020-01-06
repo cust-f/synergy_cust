9cf515051240e84e218d1b3f631e19bea0bb35c6
zjw781462408 <781462408@qq.com>
2020-01-06 09:18:58 +0800

    赵家伟 1-6-1.00

--- src/views/company/supplyBussess.vue
+++ src/views/company/supplyBussess.vue
@@ -15,35 +15,116 @@
       </div>
         </el-aside>
         <el-main>
-            <h3>供应商详情</h3>
+            <h3>全部供应商信息</h3>
              &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+              <div>
 
 
+    <div class="container">
+      <div class="handle-box">
+        <el-button
+          type="primary"
+          icon="el-icon-delete"
+          class="handle-del mr10"
+          @click="delAllSelection"
+        >批量删除</el-button>
+        <!-- <el-button type="primary" class="handle-del mr10" @click="addData">新增</el-button> -->
+      </div>
 
+      <el-table
+        :data="tableData"
+        border
+        class="table"
+        ref="multipleTable"
+        header-cell-class-name="table-header"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+        <el-table-column prop="name" label="企业名称"></el-table-column>
+        <el-table-column prop="Business_Name" label="供应商联络人"></el-table-column>
+        <el-table-column prop="Business_Tel" label="办公电话"></el-table-column>
 
-
-
-             
-             <el-table style="width: 100%" border :data="tableData" >
-  <template v-for="(item,index) in tableHead">
-    <el-table-column :prop="item.column_name" :label="item.column_comment" :key="index" v-if="item.column_name != 'id'"></el-table-column>
-
-  </template>      
-  <el-table-column label="操作" min-width="100px" align="center">
-       <template >
+        <el-table-column prop="date" label="任务完成时间"></el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template slot-scope="scope">
+            <!-- <el-button
+              type="text"
+              icon="el-icon-edit"
+              @click="handleEdit(scope.$index, scope.row)"
+            >编辑</el-button> -->
+            <el-button
+              type="text"
+              icon="el-icon-delete"
+              class="red"
+              @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button>
           <el-button @click="supplyDetail" type="text" size="small">查看详情</el-button>
-          <el-button type="text" size="small">删除</el-button>
-       </template>
-    </el-table-column>
-</el-table>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="pagination">
+        <el-pagination
+          background
+          layout="total, prev, pager, next"
+          :current-page="query.pageIndex"
+          :page-size="query.pageSize"
+          :total="pageTotal"
+          @current-change="handlePageChange"
+        ></el-pagination>
+      </div>
+    </div>
 
-<div class="con">
-  <span class="demonstration"></span>
-  <el-pagination
-    layout="prev, pager, next"
-    :total="50">
-  </el-pagination>
-</div>
+    <!-- 编辑弹出框 -->
+    <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+      <el-form ref="form" :model="form" label-width="70px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input v-model="form.address"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveEdit">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 新增弹出框 -->
+    <!-- <el-dialog title="新增" :visible.sync="addVisible" width="50%">
+      <el-form ref="form" :model="addList" label-width="70px">
+        <el-form-item label="企业名称">
+          <el-input v-model="addList.name"></el-input>
+        </el-form-item>
+        <el-form-item label="任务类型">
+          <el-input v-model="addList.address"></el-input>
+        </el-form-item>
+        <el-form-item label="金额">
+          <el-input v-model="addList.money"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select v-model="addList.state" placeholder="请选择状态">
+            <el-option label="成功" value="成功"></el-option>
+            <el-option label="失败" value="失败"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="注册时间">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="addList.date"
+            value-format="yyyy-MM-dd"
+            style="width: 100%;"
+          ></el-date-picker>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveAdd">确 定</el-button>
+      </span>
+    </el-dialog> -->
+              </div>
 
         </el-main>
     </el-container>
@@ -58,55 +139,84 @@ export default {
   name: "supplyBussess",
   data() {
     return {
-        tableHead:[
-            {
-                column_name:"company_ID",column_comment:"供应商ID"
-            },
-            {
-                column_name:"dompany_Name",column_comment:"供应商名称"
-            },
-            {
-                column_name:"Bussess_Name",column_comment:"业务联系人"
-            },
-            {
-                column_name:"Bussess_Tel",column_comment:"业务联系电话"
-            }
-        ],
-        tableData:[{
-            company_ID:'B3001',
-            dompany_Name:'天盛公司',
-            Bussess_Name:'徐晟',
-            Bussess_Tel:'0431-886888886',
-            
+        query: {
+        pageIndex: 1,
+        pageSize: 10
+        },
+        tableData: [
+        {
+          id: 1,
+          bussessType: "车间零部件生产",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          Business_Tel:'178433222578',          
+          date: "2019-11-1"
         },
         {
-            company_ID:'B3002',
-            dompany_Name:'大鹏公司',
-            Bussess_Name:'王大鹏',
-            Bussess_Tel:'0431-95275746',
-            Fixed_Assets:'3.2亿'
+          id: 1,
+          bussessType: "卫星微型零件制作",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          state: "成功",
+          date: "2019-11-1",
+          Business_Tel:'178433222578',          
+
         },
         {
-            company_ID:'B3003',
-            dompany_Name:'熊猫公司',
-            Bussess_Name:'王聪聪',
-            Bussess_Tel:'0431-9532500746',
-            Fixed_Assets:'60亿'
+          id: 1,
+          bussessType: "卫星微型零件制作",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          state: "成功",
+          date: "2019-11-1",
+          Business_Tel:'178433222578',  
         },
-                {
-            company_ID:'B3004',
-            dompany_Name:'远山集团',
-            Bussess_Name:'李山',
-            Bussess_Tel:'0431-95275746',
-            Fixed_Assets:'312亿'
-        },        {
-            company_ID:'B3005',
-            dompany_Name:'电子微孔集成公司',
-            Bussess_Name:'许杨',
-            Bussess_Tel:'0431-481123665',
-            Fixed_Assets:'3.2亿'
-        },],
-        tenderTrendsList:[
+        {
+          id: 1,
+          bussessType: "卫星微型零件制作",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          state: "成功",
+          date: "2019-11-1",
+          Business_Tel:'178433222578',  
+        },
+         {
+          id: 1,
+          bussessType: "卫星微型零件制作",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          state: "成功",
+          date: "2019-11-1",
+          Business_Tel:'178433222578',  
+        },
+        {
+          id: 1,
+          bussessType: "卫星微型零件制作",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          state: "成功",
+          date: "2019-11-1",
+          Business_Tel:'178433222578',  
+        },
+        {
+          id: 1,
+          bussessType: "导弹装配与改革",
+          name: "长春微电子信息有限公司",
+          Business_Name:'张三',
+          state: "成功",
+          date: "2019-11-1",
+          Business_Tel:'178433222578',  
+        }
+      ],
+      addList: {
+        id: null,
+        address: "",
+        name: "",
+        money: null,
+        state: null,
+        date: null
+      },
+       tenderTrendsList:[
         { column_name: "黄河远上白云间" },
         { column_name: "九曲黄河万里沙" },
         { column_name: "君不见黄河之水天上来"},
@@ -116,6 +226,13 @@ export default {
         { column_name :"萧萧远树疏林外，一半秋山带夕阳。"},
         { column_name: "黄河西来决昆仑，咆哮万里触龙门。"}
          ],
+      multipleSelection: [],
+      editVisible: false,
+      addVisible: false,
+      pageTotal: 0,
+      form: {},
+      idx: -1,
+      id: -1
     };
   },
   methods:{
