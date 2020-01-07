fdb62a6795c66b7b2d4fb9472b2ee39230d13c59
unknown <1161697120@qq.com>
2020-01-07 19:20:15 +0800

    123

--- src/views/admin/designTask.vue
+++ src/views/admin/designTask.vue
@@ -1,109 +1,91 @@
 <template>
-<div>
+  <div>
     <el-container>
-        <el-aside width="15%">
-            <div class="backGround_0">
-        <el-card class="box-card">
-            <div slot="header" class="clearfix">
-                <span>历史记录</span>
-                <el-button style="float: right; padding: 3px 0" type="text">更多详情</el-button>
+      <el-main>
+        <div class="box">
+        <h3>设计任务</h3></div>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+        <div>
+          <div class="container">
+            <div class="handle-box">
+              <el-button
+                type="primary"
+                icon="el-icon-delete"
+                class="handle-del mr10"
+                @click="delAllSelection"
+              >批量删除</el-button>
+              <!-- <el-button type="primary" class="handle-del mr10" @click="addData">新增</el-button> -->
+              <el-input v-model="query.name" placeholder="主任务名称" class="handle-input mr10"></el-input>
+              <el-input v-model="query.state" placeholder="状态" class="handle-input mr10"></el-input>
+              <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
             </div>
-            <div v-for="o in tenderTrendsList" :key="o" class="text item">
-                {{ o }}
-            </div>  
-        </el-card>
-      </div>
-        </el-aside>
-        <el-main>
-            <h3>设计任务</h3>
-             &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
-              <div>
 
+            <el-table
+              :data="tableData"
+              border
+              class="table"
+              ref="multipleTable"
+              header-cell-class-name="table-header"
+              @selection-change="handleSelectionChange"
+            >
+              <el-table-column type="selection" width="55" align="center"></el-table-column>
+              <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+              <el-table-column prop="name" label="主任务名称"></el-table-column>
+              <el-table-column prop="subname" label="子任务名称"></el-table-column>
+              <el-table-column prop="company" label="供应商名称"></el-table-column>
 
-    <div class="container">
-      <div class="handle-box">
-        <el-button
-          type="primary"
-          icon="el-icon-delete"
-          class="handle-del mr10"
-          @click="delAllSelection"
-        >批量删除</el-button>
-        <!-- <el-button type="primary" class="handle-del mr10" @click="addData">新增</el-button> -->
-         <el-input v-model="query.name" placeholder="主任务名称" class="handle-input mr10"></el-input>
-         <el-input v-model="query.state" placeholder="状态" class="handle-input mr10"></el-input>
-        <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
-      </div>
+              <el-table-column prop="state" label="任务状态"></el-table-column>
+              <el-table-column prop="remarkstate" label="评价状态"></el-table-column>
+              <el-table-column label="图纸" width="180" align="center">
+                <el-button type="text" size="small">下载</el-button>
+              </el-table-column>
 
-      <el-table
-        :data="tableData"
-        border
-        class="table"
-        ref="multipleTable"
-        header-cell-class-name="table-header"
-        @selection-change="handleSelectionChange"
-      >
-        <el-table-column type="selection" width="55" align="center"></el-table-column>
-        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-        <el-table-column prop="name" label="主任务名称"></el-table-column>
-        <el-table-column prop="subname" label="子任务名称"></el-table-column>
-        <el-table-column prop="company" label="供应商名称"></el-table-column>
-        
-        <el-table-column prop="state" label="任务状态"></el-table-column>
-        <el-table-column prop="remarkstate" label="评价状态"></el-table-column>
-        <el-table-column  label="图纸"  width="180" align="center">
-             <el-button  type="text" size="small">下载</el-button>
-        </el-table-column>
-        
-
-
-        <el-table-column label="操作" width="180" align="center">
-          <template slot-scope="scope">
-            <!-- <el-button
+              <el-table-column label="操作" width="180" align="center">
+                <template slot-scope="scope">
+                  <!-- <el-button
               type="text"
               icon="el-icon-edit"
               @click="handleEdit(scope.$index, scope.row)"
-            >编辑</el-button> -->
-            
-          <el-button @click="remarkDetail" type="text" size="small">评价</el-button>
-          <el-button @click="Detail" type="text" size="small">查看详情</el-button>
-          </template>
-        </el-table-column>
-  <el-table-column  label="是否选择"  width="180" align="center">
-    <el-button type="success" plain>选择</el-button>
-    <el-button type="danger" plain>拒绝</el-button>
-  
-        </el-table-column>
-      </el-table>
-      <div class="pagination">
-        <el-pagination
-          background
-          layout="total, prev, pager, next"
-          :current-page="query.pageIndex"
-          :page-size="query.pageSize"
-          :total="pageTotal"
-          @current-change="handlePageChange"
-        ></el-pagination>
-      </div>
-    </div>
+                  >编辑</el-button>-->
 
-    <!-- 编辑弹出框 -->
-    <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
-      <el-form ref="form" :model="form" label-width="70px">
-        <el-form-item label="用户名">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="地址">
-          <el-input v-model="form.address"></el-input>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="editVisible = false">取 消</el-button>
-        <el-button type="primary" @click="saveEdit">确 定</el-button>
-      </span>
-    </el-dialog>
+                  <el-button @click="remarkDetail" type="text" size="small">评价</el-button>
+                  <el-button @click="Detail" type="text" size="small">查看详情</el-button>
+                </template>
+              </el-table-column>
+              <el-table-column label="是否选择" width="180" align="center">
+                <el-button type="success" plain>选择</el-button>
+                <el-button type="danger" plain>拒绝</el-button>
+              </el-table-column>
+            </el-table>
+            <div class="pagination">
+              <el-pagination
+                background
+                layout="total, prev, pager, next"
+                :current-page="query.pageIndex"
+                :page-size="query.pageSize"
+                :total="pageTotal"
+                @current-change="handlePageChange"
+              ></el-pagination>
+            </div>
+          </div>
 
-    <!-- 新增弹出框 -->
-    <!-- <el-dialog title="新增" :visible.sync="addVisible" width="50%">
+          <!-- 编辑弹出框 -->
+          <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+            <el-form ref="form" :model="form" label-width="70px">
+              <el-form-item label="用户名">
+                <el-input v-model="form.name"></el-input>
+              </el-form-item>
+              <el-form-item label="地址">
+                <el-input v-model="form.address"></el-input>
+              </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+              <el-button @click="editVisible = false">取 消</el-button>
+              <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+          </el-dialog>
+
+          <!-- 新增弹出框 -->
+          <!-- <el-dialog title="新增" :visible.sync="addVisible" width="50%">
       <el-form ref="form" :model="addList" label-width="70px">
         <el-form-item label="企业名称">
           <el-input v-model="addList.name"></el-input>
@@ -134,16 +116,11 @@
         <el-button @click="addVisible = false">取 消</el-button>
         <el-button type="primary" @click="saveAdd">确 定</el-button>
       </span>
-    </el-dialog> -->
-  </div>
-
-
-        </el-main>
+          </el-dialog>-->
+        </div>
+      </el-main>
     </el-container>
-    
-    
-</div>
-      
+  </div>
 </template>
 
 <script>
@@ -151,28 +128,18 @@ export default {
   name: "designTask",
   data() {
     return {
-        query: {
+      query: {
         pageIndex: 1,
         pageSize: 10
-        },
-        tableData: [
+      },
+      tableData: [
         {
           id: 1,
           name: "主任务名",
           remarkstate: "未评价",
           state: "待完成",
           subname: "子任务名",
-          company: "xx公司",
-          
-          
-        },
-        {
-          id: 1,
-          name: "主任务名",
-          remarkstate: "未评价",
-          state: "待完成",
-          subname: "子任务名",
-          company: "xx公司",
+          company: "xx公司"
         },
         {
           id: 1,
@@ -180,9 +147,7 @@ export default {
           remarkstate: "未评价",
           state: "待完成",
           subname: "子任务名",
-          company: "xx公司",
-          
-
+          company: "xx公司"
         },
         {
           id: 1,
@@ -190,15 +155,7 @@ export default {
           remarkstate: "未评价",
           state: "待完成",
           subname: "子任务名",
-          company: "xx公司",
-        },
-         {
-          id: 1,
-          name: "主任务名",
-          remarkstate: "未评价",
-          state: "待完成",
-          subname: "子任务名",
-          company: "xx公司",
+          company: "xx公司"
         },
         {
           id: 1,
@@ -206,7 +163,7 @@ export default {
           remarkstate: "未评价",
           state: "待完成",
           subname: "子任务名",
-          company: "xx公司",
+          company: "xx公司"
         },
         {
           id: 1,
@@ -214,8 +171,23 @@ export default {
           remarkstate: "未评价",
           state: "待完成",
           subname: "子任务名",
-          company: "xx公司",
-
+          company: "xx公司"
+        },
+        {
+          id: 1,
+          name: "主任务名",
+          remarkstate: "未评价",
+          state: "待完成",
+          subname: "子任务名",
+          company: "xx公司"
+        },
+        {
+          id: 1,
+          name: "主任务名",
+          remarkstate: "未评价",
+          state: "待完成",
+          subname: "子任务名",
+          company: "xx公司"
         }
       ],
       addList: {
@@ -226,16 +198,6 @@ export default {
         state: null,
         date: null
       },
-       tenderTrendsList:[
-        { column_name: "黄河远上白云间" },
-        { column_name: "九曲黄河万里沙" },
-        { column_name: "君不见黄河之水天上来"},
-        { column_name: "白日依山尽，黄河入海流"},
-        { column_name: "黄河落天走东海，万里尘沙入胸怀"},
-        { column_name: "明月隐高树，长河没晓天"},
-        { column_name :"萧萧远树疏林外，一半秋山带夕阳。"},
-        { column_name: "黄河西来决昆仑，咆哮万里触龙门。"}
-         ],
       multipleSelection: [],
       editVisible: false,
       addVisible: false,
@@ -243,14 +205,14 @@ export default {
       form: {},
       idx: -1,
       id: -1,
-      radio: '1'
+      radio: "1"
     };
   },
-   created() {
+  created() {
     this.getData();
   },
-  methods:{
-       getData() {
+  methods: {
+    getData() {
       //   this.tableData = res.list;
       //   this.pageTotal = tableData.length;
     },
@@ -275,7 +237,6 @@ export default {
     handleSelectionChange(val) {
       this.multipleSelection = val;
     },
-
 
     delAllSelection() {
       let length = this.multipleSelection.length;
@@ -312,32 +273,63 @@ export default {
     },
     // 分页导航
     handlePageChange(val) {},
-      /*
-      *转跳对应任务信息页面
-      */
-     remarkDetail(){
-         this.$router.push('/designTaskEvaluationDetils')
-     },
-     Detail(){
-         this.$router.push('/designCirculationTaskDetail')
-     },
-     open2() {
-        this.$message({
-          message: '恭喜你，这是一条成功消息',
-          type: 'success'
-        });
-      },
+    /*
+     *转跳对应任务信息页面
+     */
+    remarkDetail() {
+      this.$router.push("/designTaskEvaluationDetils");
+    },
+    Detail() {
+      this.$router.push("/designCirculationTaskDetail");
+    },
+    open2() {
+      this.$message({
+        message: "恭喜你，这是一条成功消息",
+        type: "success"
+      });
+    }
   }
 };
 </script>
 <style>
-.con{
-    width:500px;
-    height: 1000px;
-    margin:0 auto;
-    text-align: center
+.con {
+  width: 500px;
+  height: 1000px;
+  margin: 0 auto;
+  text-align: center;
 }
 
+.handle-box {
+  margin-bottom: 20px;
+}
+
+.handle-select {
+  width: 120px;
+}
+
+.handle-input {
+  width: 300px;
+  display: inline-block;
+}
+.table {
+  width: 100%;
+  font-size: 20px;
+}
+.red {
+  color: #ff0000;
+}
+.mr10 {
+  margin-right: 10px;
+}
+.table-td-thumb {
+  display: block;
+  margin: auto;
+  width: 40px;
+  height: 40px;
+}
+.box {
+  font-size: 24px;
+}
 </style>
 
 
--- src/views/admin/newStaff.vue
+++ src/views/admin/newStaff.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- 
  * @description: 人员管理
  * @fileName: newStaff.vue 
@@ -8,142 +7,128 @@
  * @path:  
  * @version: V1.0.5 
 !-->
-=======
-
->>>>>>> 34eebc6e9a4086fcedc0fcdf7728ba9feed8b19f
-<template>
-<div>
-    <el-container>
-        <el-aside width="15%">
-      <div class="backGround_0">
-        <el-card class="box-card">
-            <div slot="header" class="clearfix">
-                <span>历史记录</span>
-                <el-button style="float: right; padding: 3px 0" type="text">更多详情</el-button>
-            </div>
-            <div v-for="o in tenderTrendsList" :key="o" class="text item">
-                {{ o }}
-            </div>  
-        </el-card>
-
-      </div>
-        </el-aside>
-
-        <el-main>
-          <h3>人员管理</h3>
-          &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
 <template>
   <div>
-    <div class="container">
-      <div class="handle-box">
-        <el-button
-          type="primary"
-          icon="el-icon-delete"
-          class="handle-del mr10"
-          @click="delAllSelection"
-        >批量删除</el-button>
+    <el-container>
+      <el-main>
+        <div class="box">
+          <h3>人员管理</h3>
+        </div>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+        <template>
+          <div>
+            <div class="container">
+              <div class="handle-box">
+                <el-button
+                  type="primary"
+                  icon="el-icon-delete"
+                  class="handle-del mr10"
+                  @click="delAllSelection"
+                >批量删除</el-button>
 
-        <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
-        <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+                <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
+                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
 
-        <el-button type="primary" class="handle-del mr10" @click="addData">新增</el-button>
-      </div>
-<div font="16px" >
-      <el-table
-        :data="tableData"
-        border
-        class="table"
-        ref="multipleTable"
-        header-cell-class-name="table-header"
-        @selection-change="handleSelectionChange"
-      >
-        <el-table-column type="selection" width="55" align="center"></el-table-column>
-        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-        <el-table-column prop="name" label="用户名"></el-table-column>
-        <el-table-column prop="realname" label="真实姓名"></el-table-column>
-          <el-table-column prop="email" label="部门"></el-table-column>
-        <el-table-column prop="email" label="邮箱"></el-table-column>
-       <el-table-column prop="phone" label="电话"></el-table-column>
-        
+                <el-button type="primary" class="handle-del mr10" @click="addData">新增</el-button>
+              </div>
+              <div class="box">
+                <el-table
+                  :data="tableData"
+                  border
+                  class="table"
+                  ref="multipleTable"
+                  header-cell-class-name="table-header"
+                  @selection-change="handleSelectionChange"
+                >
+                  <el-table-column type="selection" width="55" align="center"></el-table-column>
+                  <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+                  <el-table-column prop="name" label="用户名"></el-table-column>
+                  <el-table-column prop="realname" label="真实姓名"></el-table-column>
+                  <el-table-column prop="email" label="部门"></el-table-column>
+                  <el-table-column prop="email" label="邮箱"></el-table-column>
+                  <el-table-column prop="phone" label="电话"></el-table-column>
 
-        <el-table-column label="操作" width="180" align="center">
-          <template slot-scope="scope">
-            <el-button
-              type="text"
-              icon="el-icon-edit"
-              @click="handleEdit(scope.$index, scope.row)"
-            >编辑</el-button>
-            <el-button
-              type="text"
-              icon="el-icon-delete"
-              class="red"
-              @click="handleDelete(scope.$index, scope.row)"
-            >删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-</div>
-      <div class="pagination">
-        <el-pagination
-          background
-          layout="total, prev, pager, next"
-          :current-page="query.pageIndex"
-          :page-size="query.pageSize"
-          :total="pageTotal"
-          @current-change="handlePageChange"
-        ></el-pagination>
-      </div>
-    </div>
+                  <el-table-column label="操作" width="180" align="center">
+                    <template slot-scope="scope">
+                      <el-button
+                        type="text"
+                        icon="el-icon-edit"
+                        @click="handleEdit(scope.$index, scope.row)"
+                      >编辑</el-button>
+                      <el-button
+                        type="text"
+                        icon="el-icon-delete"
+                        class="red"
+                        @click="handleDelete(scope.$index, scope.row)"
+                      >删除</el-button>
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </div>
+              <div class="pagination">
+                <el-pagination
+                  background
+                  layout="total, prev, pager, next"
+                  :current-page="query.pageIndex"
+                  :page-size="query.pageSize"
+                  :total="pageTotal"
+                  @current-change="handlePageChange"
+                ></el-pagination>
+              </div>
+            </div>
 
-    <!-- 编辑弹出框 -->
-    <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
-      <el-form ref="form" :model="form" label-width="70px">
-        <el-form-item label="用户名">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱">
-          <el-input v-model="form.email"></el-input>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="editVisible = false">取 消</el-button>
-        <el-button type="primary" @click="saveEdit">确 定</el-button>
-      </span>
-    </el-dialog>
+            <!-- 编辑弹出框 -->
+            <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+              <el-form ref="form" :model="form" label-width="70px">
+                <el-form-item label="用户名">
+                  <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱">
+                  <el-input v-model="form.email"></el-input>
+                </el-form-item>
+              </el-form>
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+              </span>
+            </el-dialog>
 
-    <!-- 新增弹出框 -->
-    <el-dialog title="新增" :visible.sync="addVisible" width="50%">
-      <el-form ref="form" :model="addList" label-width="70px">
-        <el-form-item label="用户名">
-          <el-input v-model="addList.name"></el-input>
-        </el-form-item>
-        <el-form-item label="真实姓名">
-          <el-input v-model="addList.realname"></el-input>
-        </el-form-item>
+            <!-- 新增弹出框 -->
+            <el-dialog title="新增" :visible.sync="addVisible" width="50%">
+              <el-form ref="form" :model="addList" label-width="70px">
+                <el-form-item label="用户名">
+                  <el-input v-model="addList.name"></el-input>
+                </el-form-item>
+                <el-form-item label="真实姓名">
+                  <el-input v-model="addList.realname"></el-input>
+                </el-form-item>
 
-        <el-form-item label="角色">
-          <el-select v-model="addList.role" placeholder="请选择角色">
-            <el-option label="管理员" value="管理员"></el-option>
-            <el-option label="设计人员" value="设计人员"></el-option>
-            <el-option label="市场人员" value="市场人员"></el-option>
-            <el-option label="流通人员" value="流通人员"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="邮箱">
-          <el-input v-model="addList.email"></el-input>
-        </el-form-item>
-        <el-form-item label="电话">
-          <el-input v-model="addList.phone"></el-input>
-        </el-form-item>
-        <el-form-item label="登录密码" prop="password">
-					<el-input v-model="addList.password"></el-input>
-				</el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="addVisible = false">取 消</el-button>
-        <el-button type="primary" @click="saveAdd">确 定</el-button>
-      </span>
-    </el-dialog>
+                <el-form-item label="角色">
+                  <el-select v-model="addList.role" placeholder="请选择角色">
+                    <el-option label="管理员" value="管理员"></el-option>
+                    <el-option label="设计人员" value="设计人员"></el-option>
+                    <el-option label="市场人员" value="市场人员"></el-option>
+                    <el-option label="流通人员" value="流通人员"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="邮箱">
+                  <el-input v-model="addList.email"></el-input>
+                </el-form-item>
+                <el-form-item label="电话">
+                  <el-input v-model="addList.phone"></el-input>
+                </el-form-item>
+                <el-form-item label="登录密码" prop="password">
+                  <el-input v-model="addList.password"></el-input>
+                </el-form-item>
+              </el-form>
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="addVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveAdd">确 定</el-button>
+              </span>
+            </el-dialog>
+          </div>
+        </template>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -178,36 +163,36 @@ export default {
       tableData: [
         {
           id: 1,
-          realname:"",
+          realname: "",
           name: "张三",
-          email:"123456@163.com",
+          email: "123456@163.com",
           role: "管理员",
           phone: "123456"
         },
         {
           id: 1,
-          realname:"",
+          realname: "",
           name: "张三",
-          email:"123456@163.com",
+          email: "123456@163.com",
           role: "管理员",
           phone: "123456"
         },
         {
           id: 1,
-          realname:"",
+          realname: "",
           name: "张三",
-          email:"123456@163.com",
+          email: "123456@163.com",
           role: "管理员",
           phone: "123456"
         }
       ],
       addList: {
         id: 1,
-          realname:"",
-          name: "",
-          email:"",
-          role: "",
-          phone: ""
+        realname: "",
+        name: "",
+        email: "",
+        role: "",
+        phone: ""
       },
       multipleSelection: [],
       editVisible: false,
@@ -216,35 +201,34 @@ export default {
       form: {},
       idx: -1,
       id: -1,
-        
-                tenderTrendsList:[
+
+      tenderTrendsList: [
         { column_name: "黄河远上白云间" },
         { column_name: "九曲黄河万里沙" },
-        { column_name: "君不见黄河之水天上来"},
-        { column_name: "白日依山尽，黄河入海流"},
-        { column_name: "黄河落天走东海，万里尘沙入胸怀"},
-        { column_name: "明月隐高树，长河没晓天"},
-        { column_name :"萧萧远树疏林外，一半秋山带夕阳。"},
-        { column_name: "黄河西来决昆仑，咆哮万里触龙门。"}
-         ],
-         addForm:{
-             realname:'',
-             loginname:"",
-             password:"",
-             realName:"",
-             email:"",
-             mobile:"",
-             role:''
-             },
-        value: ''
- 
+        { column_name: "君不见黄河之水天上来" },
+        { column_name: "白日依山尽，黄河入海流" },
+        { column_name: "黄河落天走东海，万里尘沙入胸怀" },
+        { column_name: "明月隐高树，长河没晓天" },
+        { column_name: "萧萧远树疏林外，一半秋山带夕阳。" },
+        { column_name: "黄河西来决昆仑，咆哮万里触龙门。" }
+      ],
+      addForm: {
+        realname: "",
+        loginname: "",
+        password: "",
+        realName: "",
+        email: "",
+        mobile: "",
+        role: ""
+      },
+      value: ""
     };
   },
-   created() {
+  created() {
     this.getData();
   },
-  methods:{
-     // 获取 easy-mock 的模拟数据
+  methods: {
+    // 获取 easy-mock 的模拟数据
     getData() {
       //   this.tableData = res.list;
       //   this.pageTotal = tableData.length;
@@ -305,33 +289,31 @@ export default {
     },
     // 分页导航
     handlePageChange(val) {},
-      
-      /*
-      *转跳对应任务信息页面
-      */
-    
-     addstaff:function()
-     {
-         this.addFormVisible=true;
 
-     },
-     open() {
-        this.$message({
-          showClose: true,
-          message: '提交成功',
-          type: 'success'
-        });
-      },
+    /*
+     *转跳对应任务信息页面
+     */
+
+    addstaff: function() {
+      this.addFormVisible = true;
+    },
+    open() {
+      this.$message({
+        showClose: true,
+        message: "提交成功",
+        type: "success"
+      });
+    }
   }
 };
 </script>
 
 <style scoped>
-.con{
-    width:500px;
-    height: 1000px;
-    margin:0 auto;
-    text-align: center
+.con {
+  width: 500px;
+  height: 1000px;
+  margin: 0 auto;
+  text-align: center;
 }
 
 .handle-box {
@@ -348,7 +330,7 @@ export default {
 }
 .table {
   width: 100%;
-  font-size: 14px;
+  font-size: 20px;
 }
 .red {
   color: #ff0000;
@@ -361,6 +343,9 @@ export default {
   margin: auto;
   width: 40px;
   height: 40px;
+}
+.box {
+  font-size: 24px;
 }
 </style>
 
