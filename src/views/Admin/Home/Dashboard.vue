df03071bbcb4d2de1e4ef093d552835a749f55d0
dzr0014 <952404799@qq.com>
2020-01-08 14:35:16 +0800

    数据统计图表改动

--- src/views/Admin/Home/Dashboard.vue
+++ src/views/Admin/Home/Dashboard.vue
@@ -215,7 +215,7 @@ export default {
     // 指定图表的配置项和数据
     var option = {
     title: {
-        text: '企业发布的需求量Top5 ',
+        text: '核心企业发布需求量Top5 ',
         subtext: '数据来自大数据统计'
     },
     tooltip: {
