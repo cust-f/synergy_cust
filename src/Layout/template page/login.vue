a8f7c645a9fdb2e556dcca56f2dc22f63133b712
cust_f <1060823651@qq.com>
2020-01-14 14:54:20 +0800

    ......

--- src/Layout/template page/login.vue
+++ src/Layout/template page/login.vue
@@ -59,7 +59,7 @@ export default {
           that
             .axios({
               method: "post",
-              url: "http://127.0.0.1:8081/user/login",
+              url: "/api/user/login",
               data: data
             })
             .then(response => {
