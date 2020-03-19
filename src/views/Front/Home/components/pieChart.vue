42699e123ed0836488d97cc4d3a46a1374285f21
cust_f <1060823651@qq.com>
2020-03-19 18:35:04 +0800

    修改首页样式及数据统计交互

---
+++ src/views/Front/Home/components/pieChart.vue
@@ -0,0 +1,73 @@
+<template>
+                <div id="quarterlySituation" style="width: 100%;height:350%;"></div>
+</template>
+
+<script>
+export default {
+    props:{
+        pieData:{
+            type:Object
+        }
+    },
+    methods:{
+            getCharts() {
+      var charts = [];
+      var myChart = echarts.init(document.getElementById("quarterlySituation")); // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: "季度完成量",
+          subtext: " ",
+          left: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          right: 0,
+          data: 
+          // ["第一季度","第二季度","第三季度","第四季度"]
+           this.pieData.searsonCount
+        },
+        series: [
+          {
+            name: "分季完成量",
+            type: "pie",
+            radius: ["50%", "70%"],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: "center"
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: "25",
+                  fontWeight: "bold"
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: this.pieData.seasonsFinishTaskCount
+            // [
+            //   { value: 335, name: "第一季度" },
+            //   { value: 310, name: "第二季度" },
+            //   { value: 235, name: "第三季度" },
+            //   { value: 220, name: "第四季度" }
+            //   // {value: 1548, name: '搜索引擎'}
+            // ]
+          }
+        ]
+      };
+      myChart.setOption(option);
+      charts.push(myChart);
+    },
+    }
+}
+</script>
