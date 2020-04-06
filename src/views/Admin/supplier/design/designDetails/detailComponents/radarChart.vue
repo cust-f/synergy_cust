594907fa75f0c7e75ed3bc943a5619dd566e55dd
870322613 <870322613@qq.com>
2020-04-06 12:04:21 +0800

    雷达图

---
+++ src/views/Admin/supplier/design/designDetails/detailComponents/radarChart.vue
@@ -0,0 +1,67 @@
+<template>
+   <div id="charts1" style="height:100%; width:400px; float:left"></div>
+</template>
+
+
+<script>
+export default{
+    props: {
+    radarData:{
+      type:Object
+    }
+  },
+    data() {
+    return {};
+  },
+ methods: {
+//雷达图
+    getCharts1() {
+      var that = this;
+      var myChart = echarts.init(document.getElementById("charts1"));
+      
+      var option = {
+        tooltip: {},
+        legend: {
+         orient: 'vertical',
+          x: "left",
+          y: "top",
+          data: ["完成统计"]
+        },
+        radar: {
+          name: {
+            textStyle: {
+              color: "#fff",
+              backgroundColor: "#999",
+              borderRadius: 3,
+              padding: [3, 5]
+            }
+          },
+          indicator: [
+            { name: "任务时长", max: 6 },
+            { name: "计划时长", max: 6 },
+            { name: "审核时长", max: 6 },
+            { name: "申请时长", max: 2},
+            { name: "验收时长", max: 2 }
+          ]
+        },
+        series: [
+          {
+            //name:'完成统计',
+            type: "radar",
+             data: [
+              {
+                name: "完成统计",
+                value:this.radarData.radarData,
+     
+              },
+             
+            ]
+          }
+        ]
+      };
+      myChart.setOption(option);
+    },
+ 
+  }
+};
+</script>
