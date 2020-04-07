9b12234a1a7a5bab79e191e28deca6a428c8f2df
zjw781462408 <781462408@qq.com>
2020-04-07 22:37:56 +0800

    2020-4-7-1

--- src/views/Admin/personnel_allotment/designerNewList.vue
+++ src/views/Admin/personnel_allotment/designerNewList.vue
@@ -131,7 +131,7 @@ export default {
   name: "designerNewList",
   data() {
     return {
-      username1: this.$store.state.user,
+      username1: localStorage.getItem("ms_username"),
 
       pageIndex: 1,
       pageSize: 7,
