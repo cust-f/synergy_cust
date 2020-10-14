1ead86a37e781801a6b5c7a257af83073a226a55
870322613 <870322613@qq.com>
2020-10-14 20:28:38 +0800

    修订首页数据显示问题

--- src/views/Front/Home/Home.vue
+++ src/views/Front/Home/Home.vue
@@ -24,7 +24,7 @@
           <ul>
             <li>
               <span class="dataCSS">
-                本月成交任务:
+                成交任务:
                 <font class="fontStyle">{{formMonth.monthCount}}</font>项
               </span>
             </li>
@@ -603,16 +603,16 @@ export default {
       technicalIndex: "",
       org: "",
 
-      //本月数据统计
+      //数据统计-本年1月1日 到 现在时间
       formMonth: {
         monthCount: "",
         desingMonthCount: [],
         circulaterMonthCount: [],
         companyCount: [],
-        monthCountRing: "",
-        desingMonthCountRing: [],
-        circulaterMonthCountRing: [],
-        companyCountRing: [],
+        // monthCountRing: "",
+        // desingMonthCountRing: [],
+        // circulaterMonthCountRing: [],
+        // companyCountRing: [],
       },
     };
   },
@@ -626,14 +626,14 @@ export default {
     this.getStatistics();
   },
   methods: {
-    //月任务统计
+    //数据统计-本年1月1日 到 现在时间
     monthDataB() {
       var that = this;
       var data = Qs.stringify({});
       that
         .axios({
           method: "post",
-          url: "/api/findTaskMonthCount",
+          url: "/api/findData",
           data: data,
         })
         .then((response) => {
@@ -641,10 +641,10 @@ export default {
           this.formMonth.desingMonthCount = response.data.allData[1];
           this.formMonth.circulaterMonthCount = response.data.allData[2];
           this.formMonth.companyCount = response.data.allData[3];
-          this.formMonth.monthCountRing = response.data.allData[4];
-          this.formMonth.desingMonthCountRing = response.data.allData[5];
-          this.formMonth.circulaterMonthCountRing = response.data.allData[6];
-          this.formMonth.companyCountRing = response.data.allData[7];
+          // this.formMonth.monthCountRing = response.data.allData[4];
+          // this.formMonth.desingMonthCountRing = response.data.allData[5];
+          // this.formMonth.circulaterMonthCountRing = response.data.allData[6];
+          // this.formMonth.companyCountRing = response.data.allData[7];
         });
     },
 
