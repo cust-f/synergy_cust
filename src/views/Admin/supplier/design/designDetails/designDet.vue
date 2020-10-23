5ad0d1f195a8cdf03b6c78aa0f033224140f4ac7
hanhan30 <435938640@qq.com>
2020-10-23 21:52:34 +0800

    修改了两个供应方其中一个拒绝时候的BUG

--- src/views/Admin/supplier/design/designDetails/designDet.vue
+++ src/views/Admin/supplier/design/designDetails/designDet.vue
@@ -43,7 +43,7 @@
         </div>
       </div>
       <br />
-      <!-- 内部审核模块 -->
+      <!-- 内部审核模块  人员分配-->
       <div v-show="show > 1">
         <div v-show="state3 === 2">
           <Internal-Audit ref="InternalAudit"></Internal-Audit>
@@ -60,7 +60,7 @@
         <div>
           <return-File ref="returnFile"></return-File>
         </div>
-      <div v-show="show > 4" class="designDet">
+      <div v-show="show > 4&&state3 === 2" class="designDet">
         <br />
         <br />
         <div
@@ -73,7 +73,7 @@
 
         <br />
         <!-- 评价模块 -->
-        <div v-if="reMarkId === 0">
+        <div v-if="reMarkId === 0&&state3 === 2">
           <div
             class="loading1"
             v-loading="loading"
@@ -252,9 +252,17 @@ export default {
           this.taskTableData = response.data.allData.a;
           this.state = response.data.allData.a[0].taskState;
           this.state2 = response.data.allData.b[0].checkPlanState;
+         if(this.state2==2){
           this.state3 = response.data.allData.a[0].contractState;
+         }else{
+           this.state3 =0;
+         }
           this.state4 = response.data.allData.b[0].checkApplyState;
+        if(this.state2==2){
           this.designCount = response.data.allData.a[0].designCount;
+        }else{
+          this.designCount=0;
+        }
           this.sendMsg();
           this.$refs.returnFile.getMsg(response.data.allData);
           if (this.state == "申请或邀请中") {
