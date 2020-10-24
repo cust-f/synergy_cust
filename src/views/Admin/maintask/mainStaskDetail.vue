3d6b15d315eefdcbe13e4ac2b9312b8836ca7ca0
subway <870322613@qq.com>
2020-10-24 09:45:28 +0800

    】修复修改时间出现的Bug

--- src/views/Admin/maintask/mainStaskDetail.vue
+++ src/views/Admin/maintask/mainStaskDetail.vue
@@ -2409,36 +2409,6 @@ export default {
           this.WJSM = response.data.allData.SM;
           this.tableData1 = response.data.allData.b;
           console.log(response);
-          if (this.tableData1.length != 0) {
-            console.log("this.tableData1");
-            //读取所有需求的时间
-            this.timeList.publishTime = response.data.allData.a[0].publishTime;
-            this.timeList.applyTime = response.data.allData.b[0].applyTime;
-            this.timeList.checkApplyTime =
-              response.data.allData.b[0].checkApplyTime;
-            this.timeList.planUploadTime =
-              response.data.allData.b[0].planUploadTime;
-            this.timeList.checkPlanTime =
-              response.data.allData.b[0].checkPlanTime;
-            this.timeList.beginTime = response.data.allData.a[0].beginTime;
-            this.timeList.deadLine = response.data.allData.a[0].deadline;
-            this.timeList.uploadContractTime =
-              response.data.allData.a[0].uploadContractTime;
-            this.timeList.checkContractTime =
-              response.data.allData.a[0].checkContractTime;
-            this.timeList.supplierDistributionTime =
-              response.data.allData.a[0].supplierDistributionTime;
-            this.timeList.designerAcceptTime =
-              response.data.allData.a[0].designerAcceptTime;
-            this.timeList.uploadDesignTime =
-              response.data.allData.a[0].uploadDesignTime;
-            this.timeList.supplierCheckDesignTime =
-              response.data.allData.a[0].supplierCheckDesignTime;
-            this.timeList.demandorCheckDesignTime =
-              response.data.allData.a[0].demandorCheckDesignTime;
-            this.timeList.finishTime = response.data.allData.a[0].finishTime;
-            //读取时间结束
-          }
           this.cooList.shifousimi = response.data.allData.a[0].sssm;
           this.selectCateKeys[0] =
             response.data.allData.a[0].taskCategoryMainId;
