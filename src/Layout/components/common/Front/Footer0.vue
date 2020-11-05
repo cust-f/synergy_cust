b4ff59aedd2da8721608ba96268b40af4ce59e74
cust <781462408@qq.com>
2020-11-05 09:52:22 +0800

    2020-11-5-1

--- src/Layout/components/common/Front/Footer0.vue
+++ src/Layout/components/common/Front/Footer0.vue
@@ -1,7 +1,7 @@
 <template>
     
 <div class="footer_bottom">
-        <div class="footer_copy">Copyright @ 2008 长春理工大学 | 地址：中国吉林省长春市卫星路7186号(南)</div>
+        <div class="footer_copy">Copyright @ 2019 长春理工大学 | 地址：中国吉林省长春市卫星路7186号(南)</div>
         
     </div>
 </template>
