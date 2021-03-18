76fee9647bcd61c7a3528d9db50f284065f00d46
custsyz <custsyz@163.com>
2021-03-18 10:34:57 +0800

    添加【浙大工作流管理】的版权

--- src/views/Admin/Home/WorkflowManage.vue
+++ src/views/Admin/Home/WorkflowManage.vue
@@ -9,5 +9,23 @@
       style="border: 1px solid #eee"
       src="http://10.31.8.48:8083/#/flowMain"
     />
+    <br/><br/>
+    <el-row>
+        <img
+          style="width: 30px; height: 30px;float:left;"
+          src="../../../assets/img/zheda.png"
+        />
+        <p
+          style="
+            font-size: 16px;
+            color: #7d7974;
+            float: left;
+            line-height: 30px;
+            margin-left: 10px;
+          "
+        >
+        版权所有：浙江大学、国家重点研发计划项目支持
+        </p>
+    </el-row>
   </div>
 </template>
