2d6abf53b425a5f2bb1bca403eea950464cc5482
“custsyz <custsyz@163.com>
2021-05-08 09:43:07 +0800

    流通任务 申请列表操作后（通过/拒绝）刷新

--- src/views/Admin/supplier/assembly/applicationInformation.vue
+++ src/views/Admin/supplier/assembly/applicationInformation.vue
@@ -383,6 +383,7 @@ export default {
             })
             .then((response) => {
               this.$message.success(checkTip + "申请成功");
+              this.$router.go(0);
             });
         })
         .catch(() => {
