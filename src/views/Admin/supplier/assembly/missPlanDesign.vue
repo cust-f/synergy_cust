d278de4f211c04284760a24f4de58b230aa59070
“custsyz <custsyz@163.com>
2021-05-24 09:28:14 +0800

    设计供应方-计划拒绝原因 提示后延时刷新

--- src/views/Admin/supplier/assembly/missPlanDesign.vue
+++ src/views/Admin/supplier/assembly/missPlanDesign.vue
@@ -91,22 +91,18 @@
     </el-dialog>
 
     <!-- 任务计划拒绝原因 -->
-    <el-dialog :visible.sync="addVisible2" width="50%">
-      <div class="biaoti" style="padding: 0 10px; border-left: 3px solid #4e58c5;">拒绝原因</div>
-      <br />
+    <el-dialog :visible.sync="addVisible2" width="50%" class="refuseDialog">
+      <div class="biaoti" style="padding: 0 10px; border-left: 3px solid #4e58c5;">计划拒绝原因</div>
       <br />
       <el-row>
         <el-col :span="8"></el-col>
       </el-row>
-      <el-form ref="form" :model="addList2" label-width="120px">
+      <el-form ref="form" :model="addList2" label-width="0px">
         <el-row>
-          <el-col>
-            <el-input
-              type="textarea"
-              :autosize="{ minRows: 5, maxRows: 7}"
-              v-model="addList2.refusePlanMessage"
-              :readonly="true"
-            ></el-input>
+          <el-col :span="24" style="width:100%">
+            <el-form-item>
+              <el-input type="textarea" rows="3" v-model="addList2.refusePlanMessage" :readonly="true" placeholder="已拒绝"></el-input>
+            </el-form-item>
           </el-col>
         </el-row>
       </el-form>
@@ -368,7 +364,7 @@ export default {
         this.technicalFile[this.shangchuancishu] +
         "linklink";
       this.shangchuancishu = this.shangchuancishu + 1;
-      this.$notify.success({
+      this.$message.success({
         title: "成功",
         message: `文件上传成功`
       });
@@ -387,7 +383,8 @@ export default {
         .then(response => {
           this.technicalFileWanzheng = "";
         });
-      this.$router.go(0);
+      // this.$router.go(0);
+      setTimeout(() => {this.$router.go(0);}, 1000);
     },
     handleExceed(){
       this.$message.info("只允许上传一个文件");
@@ -422,5 +419,13 @@ export default {
   .el-upload--text{
     border: 0px;
   }
+}
+</style>
+<style>
+.refuseDialog .el-dialog__body {
+  padding-bottom: 0px;
+}
+.refuseDialog .el-dialog__footer {
+  padding-top: 0px;
 }
 </style>
